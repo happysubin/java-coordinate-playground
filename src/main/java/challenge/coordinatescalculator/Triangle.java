fae60d225607207b7642644eb6808e85364a2e63
happysubin <asb0711@gmail.com>
2022-08-14 15:57:19 +0900

    Feat: Triangle getInfo, calculate 로직 구현

--- src/main/java/challenge/coordinatescalculator/Triangle.java
+++ src/main/java/challenge/coordinatescalculator/Triangle.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 public class Triangle extends AbstractShape {
 
+    public final static String PRINT_LINE_DISTANCE_TEXT = "삼각형 넓이는 ";
+
     public final static String UNSUPPORTED_METHOD_TRIANGLE = "TRIANGLE 클래스에서는 지원하지 않는 메서드입니다.";
 
     public Triangle(List<Position> positions) {
@@ -12,12 +14,25 @@ public class Triangle extends AbstractShape {
 
     @Override
     public double calculateArea() {
-        return 0;
+        List<Position> positions = getPositions();
+        double firstXDistance = Math.pow(positions.get(0).getX() - positions.get(1).getX(), 2);
+        double firstYDistance = Math.pow(positions.get(0).getY() - positions.get(1).getY(), 2);
+        double secondXDistance = Math.pow(positions.get(1).getX() - positions.get(2).getX(), 2);
+        double secondYDistance = Math.pow(positions.get(1).getY() - positions.get(2).getY(), 2);
+        double thirdXDistance = Math.pow(positions.get(2).getX() - positions.get(0).getX(), 2);
+        double thirdYDistance = Math.pow(positions.get(2).getY() - positions.get(0).getY(), 2);
+        double firstResult = Math.sqrt(firstXDistance + firstYDistance);
+        double secondResult = Math.sqrt(secondXDistance + secondYDistance);
+        double thirdResult = Math.sqrt(thirdXDistance + thirdYDistance);
+
+        double s = (firstResult + secondResult + thirdResult) / 2;
+        double area = Math.sqrt( s * (s - firstResult) * (s - secondResult) * ( s - thirdResult));
+        return Math.round(area); //반 올림
     }
 
     @Override
     public String getDistanceInfo() {
-        return null;
+        return PRINT_LINE_DISTANCE_TEXT + calculateArea();
     }
 
     @Override
--- src/test/java/challenge/coordinatescalculator/TriangleTest.java
+++ src/test/java/challenge/coordinatescalculator/TriangleTest.java
@@ -6,6 +6,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.offset;
 
 public class TriangleTest {
 
@@ -19,5 +20,18 @@ public class TriangleTest {
 
         //then
         assertThat(shape.getPositions().size()).isEqualTo(3);
+    }
+
+    @Test
+    void calculateTest(){
+        //given
+        List<Position> positions = Arrays.asList(new Position(10,10), new Position(14, 15), new Position(20, 8));
+        Shape shape = ShapeGroup.getShape(positions.size(), positions);
+
+        //when, then
+        String distanceInfo = shape.getDistanceInfo();
+
+        //then
+        assertThat(distanceInfo).isEqualTo("삼각형 넓이는 29.0");
     }
 }
